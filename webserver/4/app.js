1ffdb2eb26b7870ffd54a311ef324ba209c34a0f
Ionut Vizitiu <ivizitiu@ganintegrity.com>
2017-08-08 21:56:48 +0200

    serving html file as a stream (chunk of data)

---
+++ webserver/4/app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
+}).listen(1337, '127.0.0.1');
