36d7a5dcf83876b2d2b383d14ef39e56ae071b5f
Ionut Vizitiu <ivizitiu@ganintegrity.com>
2017-08-08 22:30:25 +0200

    routing

---
+++ webserver/6/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const obj = {
+      firstName: 'john',
+      lastName: 'doe'
+    };
+
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+
+}).listen(1337, '127.0.0.1');
