0b0e30b8f787c5ab80f4512472282eaf1246a16a
Ionut Vizitiu <ivizitiu@ganintegrity.com>
2017-08-31 21:25:56 +0200

    routing with parameter

---
+++ express/2/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3093;
+
+app.get('/', (req, res) => {
+  res.send('<html><head></head><body><h1>Hello</h1></body></html>');
+});
+
+app.get('/person/:id', (req, res) => {
+  res.send(`<html><head></head><body><h1>Hello ${req.params.id}</h1></body></html>`);
+});
+
+app.get('/api', (req, res) => {
+  res.json({ first: 'john', last: 'doe' });
+});
+
+app.listen(port);
